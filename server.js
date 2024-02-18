dfd6fd5697ef0083c7766f2cfad5363b0e89c833
LNJaideep2002 <87214924+LNJaideep2002@users.noreply.github.com>
2024-02-18 10:27:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ const cors=require("cors")
 app.use(cors())
 const server=http.createServer(app)
 
-const io=new Server(server,{
-    cors:{
-        origin:"http://localhost:3000",
-        methods:["GET","POST"],
-    },
-})
-io.on("connection",(socket)=>
-{
-    console.log(`User Connected :${socket.id}`)
-    socket.on("send_message",(data)=>
-    {
-        socket.broadcast.emit("receive_message",data)
-    });
-})
+// const io=new Server(server,{
+//     cors:{
+//         origin:"http://localhost:3000",
+//         methods:["GET","POST"],
+//     },
+// })
+// io.on("connection",(socket)=>
+// {
+//     console.log(`User Connected :${socket.id}`)
+//     socket.on("send_message",(data)=>
+//     {
+//         socket.broadcast.emit("receive_message",data)
+//     });
+// })
 app.get("/get",(req,res)=>
 {
     res.send("data sent")
